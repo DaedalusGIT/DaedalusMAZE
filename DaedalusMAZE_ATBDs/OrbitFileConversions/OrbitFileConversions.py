d5cb5bade6e7d58ebaec4394512609b5d9402f0a
balukid <b.dimitris@gmail.com>
2020-05-08 22:08:27 +0300

    corrections

--- DaedalusMAZE_ATBDs/OrbitFileConversions/OrbitFileConversions.py
+++ DaedalusMAZE_ATBDs/OrbitFileConversions/OrbitFileConversions.py
@@ -5,20 +5,22 @@ import Conversions as Conversions
 import pandas as pd
 import apexpy as ap
 import numpy as np
+import time
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import calendar
 import csv
 
 def parseDaedalusDate( dateString ):
     result = None
     try:
-        result = datetime.strptime(dateString[0:24], '%b %d %Y %H:%M:%S.%f')
+        result = datetime.strptime(dateString, '%d %b %Y %H:%M:%S.%f')
     except:
         try:
             result = datetime.strptime(dateString, '%b %d %Y %H:%M:%S.%f')
         except:
             try:
-                result = datetime.strptime(dateString, '%d %b %Y %H:%M:%S.%f')
+                result = datetime.strptime(dateString[0:24], '%b %d %Y %H:%M:%S.%f')                
             except:
                 result = None
     return result
@@ -37,30 +39,43 @@ and creates a new csv orbit file with the same fields plus the extra fields:
 The values are calculated for modified apex at 90 km - used by TIEGCM as well
 '''
 def AddMagneticCoordinates( sourceFilename, resultFilename ):
+    startSecs = time.time()
+    prev_time = current_time = None
+    timestep  = 0 
+    linenum   = 0
     CSVfileOUT = open(resultFilename, 'w')
     CSVwriter  = csv.writer(CSVfileOUT, delimiter=',')
-    CSVwriter.writerow( ["Time (UTCG)", "Lat (deg)", "Lon (deg)", "Alt (km)", "Daedalus.Magnetic Latitude", "Daedalus.Magnetic Longitude", "Daedalus.MLT"] )
+    CSVwriter.writerow( ["Epoch(UTCG)", "Lat_GEOD(deg)", "Lon_GEOD(deg)", "Height_WGS84 (km)", "Daedalus.Magnetic Latitude", "Daedalus.Magnetic Longitude", "Daedalus.MLT"] )
     with open( sourceFilename ) as CSVfileIN:        
         CSVreader = csv.reader( CSVfileIN )
         # locate the column numnbers of interest inside the csv file
         CSVheader = next( CSVreader )
-        Time_idx     = CSVheader.index( "Time (UTCG)" ) #CSVheader.index( "Daedalus.EpochText" )
-        Lat_idx      = CSVheader.index( "Lat (deg)" ) #CSVheader.index( "Daedalus.Latitude" )
-        Lon_idx      = CSVheader.index( "Lon (deg)" ) #CSVheader.index( "Daedalus.Longitude" )
-        Alt_idx      = CSVheader.index( "Alt (km)" ) #CSVheader.index( "Daedalus.Longitude" )
+        Time_idx     = CSVheader.index( "Epoch(UTCG)" ) #CSVheader.index( "Daedalus.EpochText" )
+        Lat_idx      = CSVheader.index( "Lat_GEOD(deg)" ) #CSVheader.index( "Daedalus.Latitude" )
+        Lon_idx      = CSVheader.index( "Lon_GEOD(deg)" ) #CSVheader.index( "Daedalus.Longitude" )
+        Alt_idx      = CSVheader.index( "Height_WGS84 (km)" ) #CSVheader.index( "Daedalus.Longitude" )
         # read the orbit file
         for row in CSVreader: # for each satellite position
+            if len(list(row))==0: break # <<<<
+            linenum += 1
             resultItems = list()
             # add the standard fields to the result file
             resultItems.append( row[Time_idx] )
             resultItems.append( row[Lat_idx] ) # Latitude is geodetic inside the orbit file
             resultItems.append( row[Lon_idx] ) 
             resultItems.append( row[Alt_idx] )
-            # Calculate the extra fields            
+            # Calculate the extra fields    
+            prev_time    = current_time
             current_time = parseDaedalusDate( row[Time_idx] )  # read time for the current orbit position 
             if current_time == None:
-                print( "ERROR - Wrong time format:", row[Time_idx] )
-                return                
+                print( "ERROR - Wrong time format at line", linenum, ":", row[Time_idx] )
+                continue       
+            if prev_time is not None:
+                if timestep==0:
+                    timestep = calendar.timegm(current_time.utctimetuple()) - calendar.timegm(prev_time.utctimetuple())
+                else:
+                    if calendar.timegm(current_time.utctimetuple()) - calendar.timegm(prev_time.utctimetuple()) != timestep:
+                        print( "Time leap at line", linenum, ":", row, "(", calendar.timegm(current_time.utctimetuple()) - calendar.timegm(prev_time.utctimetuple()) ,"sec)", "\n", prev_time, "\n", current_time )
             # take care of time so that it is compatible with igrf
             #if current_time.year > 2024:  
             #    time_for_igrf = current_time - relativedelta(years=13)
@@ -76,6 +91,8 @@ def AddMagneticCoordinates( sourceFilename, resultFilename ):
             CSVwriter.writerow( resultItems )
     # clean up
     CSVfileOUT.close()
+    finishSecs = time.time()
+    print( "Processed", linenum, "lines in" , finishSecs-startSecs, "sec")
 
     
 '''
@@ -89,7 +106,6 @@ and creates a new csv orbit file with the same fields plus the extra fields:
     Daedalus.Magnetic Longitude
     Daedalus.MLT
 The values are calculated for quasi-dipole approximation
-'''
 def AddMagneticCoordinates_QuasiDipole( sourceFilename, resultFilename ):
     input_file = pd.read_csv( sourceFilename ) 
     geod_lat = np.array(input_file["Lat (deg)"])
@@ -148,7 +164,7 @@ def AddMagneticCoordinates_QuasiDipole( sourceFilename, resultFilename ):
     input_file['Daedalus.MLT']=MLT
 
     input_file.to_csv( resultFilename, index=None )
-
+'''
 #new_file_dict = {"Epoch(UTCG)":new_time_string,'DAY_OF_YEAR':NEW_DOY, 'Lat_MAG (deg)':MAG_LAT, 'Lon_MAG (deg)':MAG_LON, 'Alt_MAG (km)':new_alt,'Magnetic Local Time (hours)':MLT}
 #df = pd.DataFrame(new_file_dict)
 #df.to_csv('Deadalus Orbits/DAED_ORB_LIFETIME_93deg_QD_'+str(step)+'mins_step.csv', index=False)
